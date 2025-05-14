3bea8db59f557eb16c57bad73d132b6ce5a2a997
cmrgvu <zuno.verghese@vidyashilp.edu.in>
2025-05-14 14:26:20 +0000

    Deploying to gh-pages from @ cmrgvu/cmrgvu.github.io@72c2e2886e6b42ad7b8c0ab3ba7ce15c28ef1157 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/study/";
           },
-        },{id: "nav-team",
-          title: "Team",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/team/";
-          },
         },{id: "nav-what-we-do",
           title: "What We Do",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/What%20We%20Do/";
+          },
+        },{id: "nav-team",
+          title: "Team",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/team/";
           },
         },{id: "dropdown-regulatory-design",
               title: "Regulatory Design",
