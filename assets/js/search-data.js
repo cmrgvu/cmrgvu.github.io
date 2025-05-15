9abc28c5c0995646ca5de2844f621f9d70136e2b
cmrgvu <zuno.verghese@vidyashilp.edu.in>
2025-05-15 05:06:03 +0000

    Deploying to gh-pages from @ cmrgvu/cmrgvu.github.io@349581ff259af7e53d8eea961f2bb2a332affc24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/What%20We%20Do/";
           },
-        },{id: "nav-study",
-          title: "Study",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
